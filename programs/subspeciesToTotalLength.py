ce1a9cf8d4fe9efa7c27436ce2c44f518496a41a
JeremyCoxBMI <jeremy.cox.cs@gmail.com>
2018-03-17 22:21:08 -0400

    New program

--- programs/subspeciesToTotalLength.py
+++ programs/subspeciesToTotalLength.py
@@ -4,7 +4,7 @@ __author__ = 'COX1KB'
 import sys
 
 #data structure
-# maps speciesID to, DICT firstTaxon to length)
+# maps speciesID to, DICT firstTaxon to length)  
 
 speciesToFirstTaxonLength = {}
 
@@ -18,7 +18,7 @@ for line in open(sys.argv[1]):
       firstTaxon = splits[2]
       speciesTaxon = splits[4]
       length = int(splits[8])
-
+    
       if not speciesTaxon in speciesToFirstTaxonLength:
 	  speciesToFirstTaxonLength[speciesTaxon] = {}
       specDict = speciesToFirstTaxonLength[speciesTaxon]
@@ -30,9 +30,22 @@ for line in open(sys.argv[1]):
 
 
 outF = open(sys.argv[1]+".species.to.length", "w")
-outF.write("SPECIES_IT\tFIRST_TAXON\tLENGTH\n")
+outF.write("SPECIES_ID\tFIRST_TAXON\tLENGTH\n")
+outF2 = open(sys.argv[1]+".one_species.to.length", "w")
+outF2.write("SPECIES_ID\tFIRST_TAXON\tLENGTH\n")
 
+all_total=0
+total=0
 for specID in speciesToFirstTaxonLength:
     firstDict = speciesToFirstTaxonLength[specID]
+    maxLength = (0,"fakeName")
     for f in firstDict:
-        outF.write(specID+"\t"+f+"\t"+str(firstDict[f])+"\n")
+      if maxLength[0] < firstDict[f]:
+	 maxLength = (firstDict[f],f)
+      outF.write(specID+"\t"+f+"\t"+str(firstDict[f])+"\n")
+      all_total += firstDict[f]
+    outF2.write(specID+"\t"+maxLength[1]+"\t"+str(maxLength[0])+"\n")
+    total += maxLength[0]
+
+print "total length of Bacteria Metagenome per species is "+str(all_total)
+print "total length using longest genome per species only is "+str(total)
